3108e915127ff968b95cd8218e0b9657e9dc66fc
Daniel Leite de Oliveira <dleitee@users.noreply.github.com>
2017-02-13 23:21:24 -0200

    Add plugin source

---
+++ src/index.js
@@ -0,0 +1,12 @@
+'use strict'
+
+module.exports = function() {
+  return {
+    name: 'transform-remove-import',
+    visitor: {
+      ImportDeclaration(path) {
+        path.remove()
+      },
+    },
+  }
+}
